96bc2bdd22023a6070bdaef3462189f5fa6b2e02
abhinandan-verma <abhinandanverma551@gmail.com>
2024-08-31 13:29:49 +0530

    changed server code

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
-import multer from "multer";
 import router from "./src/routers/item.routers.js";
 import dbConnect from "./src/lib/dbConnect.js";
 
 const app = express();
 
-const storage = multer.memoryStorage();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello App is running:)" });
 });
 
+// Mount the router on a specific route
+app.use("/api", router);
+
 dbConnect()
 .then(() => {
     app.listen(process.env.PORT || 3000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT || 3000}`);
-    })
+    });
 
     app.post('/webhook', (req, res) => {
         const { process, status, message } = req.body;
@@ -33,14 +33,10 @@ dbConnect()
     app.get('/webhook', (req, res) => {
         const { process, status, message } = req.body;
         console.log(`Process: ${process}, Status: ${status}, Message: ${message}`);
-
         res.status(200).json({ message: "You are on webhook api :)" });
-        
     });
-    
 })
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
     return res.status(500).json({ error: "Internal Server Error, Try Again " });
-})
-
+});
