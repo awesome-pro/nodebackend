dc3199493c1e084e1efa5f3d467af4400b80af86
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-01 15:04:29 +0530

    api post fix

--- src/routers/item.routers.js
+++ src/routers/item.routers.js
@@ -10,4 +10,8 @@ router.route("/post").post(upload.single("file"), (req, res) => {
     processCSV(req, res);
 });
 
+router.route("/post").get((req, res) => {
+    res.json({ status: "Congrtats! api/post API, use postman to send CSV file for process :)" });
+});
+
 export default router;
